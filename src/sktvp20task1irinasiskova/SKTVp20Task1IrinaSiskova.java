0ebf8278091ed551c65adb11ec85c3633a387d66
irina Siskova <Irina.Siskova@ivkhk.ee>
2021-09-28 18:47:46 +0300

    Задание1

---
+++ src/sktvp20task1irinasiskova/SKTVp20Task1IrinaSiskova.java
@@ -0,0 +1,12 @@
+
+package sktvp20task1irinasiskova;
+
+
+public class SKTVp20Task1IrinaSiskova {
+
+   
+    public static void main(String[] args) {
+        System.out.println("Java Hello!");
+    }
+    
+}
